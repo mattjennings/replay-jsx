9d82a8bb942258a961060413e46c0f9c76b308e0
Matt Jennings <mattjennings44@gmail.com>
2020-07-05 20:41:09 -0500

    cleanup

--- src/bird.tsx
+++ src/bird.tsx
@@ -6,8 +6,6 @@ export const birdHeight = 40;
 export const Bird = makeSprite({
   render() {
     return (
-      // fragments could be used to wrap JSX instead of arrays? just an idea
-      // but wrapping in an array works too
       <>
         <image fileName="bird.png" width={birdWidth} height={birdHeight} />
       </>
